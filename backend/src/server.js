f0056d5a77b6e16fe88a20f2db7cfb8220f15a38
Akashsahani47 <ak676964@gmail.com>
2026-02-02 19:27:02 +0530

    done

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 
